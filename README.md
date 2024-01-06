7477cb485c6b75271fc46e0b5661f8d1fc5841e2
matyb08 <suricate66@protonmail.com>
2024-01-06 16:29:26 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# basic
+# setup
+
+## virtualenv
+_used python 3.10_
+```shell
+python3 -m venv .venv
+.venv/Scripts/activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+```
+
+## host
+
 - Add project folder to PATH
-- `sudo apt install -y yt-dlp ffmpeg`
+- install ffmpeg and yt-dlp
+  ```shell
+  sudo apt install -y ffmpeg
+  sudo python3 -m pip install yt-dlp
+  ```
 - alias to `mm` 😉
--- musicmanager.py
+++ musicmanager.py
@@ -1,36 +1,36 @@
 import os
+import pathlib
 import subprocess
 import slugify
 import logging
 import json
 import argparse
-import sys
 from PIL import Image
+
+PROJECT_ROOT = pathlib.Path(__file__).parent.resolve()
 
 
 class Playlist:
-    savePathRoot = None
-
-    def __init__(self, link, downloadWholePlaylist=True, name=None):
-        if not self.savePathRoot:
-            raise Exception('\'savePathRoot\' must be defined.')
-
+    def __init__(self, link, save_path_root='./', download_whole_playlist=True, name=None):
+        self.save_path_root = save_path_root
         self.name = name
         self.link = link
-        self.downloadWholePlaylist = downloadWholePlaylist
+        self.download_whole_playlist = download_whole_playlist
         if name:
-            self.savePath = os.path.join(self.savePathRoot, name)
+            self.save_path = os.path.join(self.save_path_root, name)
         else:
-            self.savePath = self.savePathRoot
+            self.save_path = self.save_path_root
 
         if self.name:
-            logName = f'.{slugify.slugify(self.name)}-downloaded-archive.txt'  # todo check
-            self.downloadArchivePath = os.path.join(self.savePath, logName)
+            self.download_archive_path = os.path.join(
+                self.save_path,
+                f'.{slugify.slugify(self.name)}-downloaded-archive.txt'
+            )
         else:
-            self.downloadArchivePath = os.path.join(self.savePath, '.downloaded-archive.txt')
+            self.download_archive_path = os.path.join(self.save_path, '.downloaded-archive.txt')
 
-        if not os.path.exists(self.savePath):
-            os.makedirs(self.savePath)
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
 
     def download(self):
         subprocess.run([
@@ -43,33 +43,33 @@ class Playlist:
             '--ignore-errors',
             '--add-metadata',
             '--download-archive',
-            self.downloadArchivePath,
-            '--yes-playlist' if self.downloadWholePlaylist else '--no-playlist',
+            self.download_archive_path,
+            '--yes-playlist' if self.download_whole_playlist else '--no-playlist',
             '--no-post-overwrites',
             '--embed-thumbnail',
             '--output',
-            os.path.join(self.savePath, '+%(title)s %(id)s.%(ext)s'),
+            os.path.join(self.save_path, '+%(title)s %(id)s.%(ext)s'),
             self.link
         ])
 
-        logger.info(f'Download to {os.path.abspath(self.savePath)} finished.')
+        musicmanager_logger.info(f'Download to {os.path.abspath(self.save_path)} finished.')
 
-    def fixAlbumArt(self):
-        for fileName in os.listdir(self.savePath):
-            if fileName.startswith('+') and fileName.endswith('.mp3'):
+    def fix_album_art(self):
+        for file_name in os.listdir(self.save_path):
+            if file_name.startswith('+') and file_name.endswith('.mp3'):
                 # Extract the image from mp3 file
                 subprocess.run([
                     'ffmpeg',
                     '-i',
-                    os.path.join(self.savePath, fileName),
+                    os.path.join(self.save_path, file_name),
                     '-an',
                     '-c:v',
                     'copy',
-                    os.path.join(self.savePath, 'tmp-art-uncropped.jpg')
+                    os.path.join(self.save_path, 'tmp-art-uncropped.jpg')
                 ])
 
                 # Crop & save the image
-                im = Image.open(os.path.join(self.savePath, 'tmp-art-uncropped.jpg'))
+                im = Image.open(os.path.join(self.save_path, 'tmp-art-uncropped.jpg'))
                 width, height = im.size
 
                 left = int((width - height) / 2)
@@ -78,15 +78,15 @@ class Playlist:
                 lower = height
 
                 im = im.crop((left, upper, right, lower))
-                im.save(os.path.join(self.savePath, 'tmp-art-cropped.jpg'))
+                im.save(os.path.join(self.save_path, 'tmp-art-cropped.jpg'))
 
                 # Embed the image
                 subprocess.run([
                     'ffmpeg',
                     '-i',
-                    os.path.join(self.savePath, fileName),
+                    os.path.join(self.save_path, file_name),
                     '-i',
-                    os.path.join(self.savePath, 'tmp-art-cropped.jpg'),
+                    os.path.join(self.save_path, 'tmp-art-cropped.jpg'),
                     '-map',
                     '0:0',
                     '-map',
@@ -96,105 +96,123 @@ class Playlist:
                     '-id3v2_version',
                     '3',
                     '-metadata:s:v',
-                    'title=Album cover',  # vzel sm ""
+                    'title=Album cover',
                     '-metadata:s:v',
-                    'comment=Cover (front)',  # vzel sm ""
-                    os.path.join(self.savePath, fileName[1:])
+                    'comment=Cover (front)',
+                    os.path.join(self.save_path, file_name[1:])
                 ])
 
                 # Clean up
-                os.remove(os.path.join(self.savePath, 'tmp-art-uncropped.jpg'))
-                os.remove(os.path.join(self.savePath, 'tmp-art-cropped.jpg'))
-                os.remove(os.path.join(self.savePath, fileName))
+                os.remove(os.path.join(self.save_path, 'tmp-art-uncropped.jpg'))
+                os.remove(os.path.join(self.save_path, 'tmp-art-cropped.jpg'))
+                os.remove(os.path.join(self.save_path, file_name))
 
-                logger.info(f'Fixed album art in {os.path.abspath(self.savePath)}')
+                musicmanager_logger.info(f'Fixed album art in {os.path.abspath(self.save_path)}')
 
 
-if __name__ == '__main__':
-    # LOGGING SETUP
+def setup_logger():
     logger = logging.getLogger('logger')
     logger.setLevel(logging.INFO)
 
-    fileHandler = logging.FileHandler('logger.log')
-    fileHandler.setLevel(logging.INFO)
+    file_handler = logging.FileHandler(os.path.join(PROJECT_ROOT, 'logger.log'))
+    file_handler.setLevel(logging.INFO)
 
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fileHandler.setFormatter(formatter)
-    consoleHandler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
 
-    logger.addHandler(fileHandler)
-    logger.addHandler(consoleHandler)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
-    # ARGUMENT PARSING. for configurations made at command line, ignoring config file
-    argParser = argparse.ArgumentParser(
+    return logger
+
+
+def get_config():
+    with open(os.path.join(PROJECT_ROOT, 'config.json'), 'r') as f:
+        return json.load(f)
+
+
+def get_args():
+    arg_parser = argparse.ArgumentParser(
         description='Download YouTube Music and other content hassle-free.',
-        epilog='Example: python3 musicmanager.py ...neki...'  # todo
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog='''
+Examples:
+python3 musicmanager.py
+python3 musicmanager.py quick
+python3 musicmanager.py so 'https://music.youtube.com/watch?v=0hnwIedsoNI'
+ 
+'''
     )
 
     actions = (
-        'quick',
-        'q',
-        'playlist',
-        'pl',
-        'song',
-        'so',
-    ) # shorter ones are aliases
-    argParser.add_argument(
+        'quick', 'q',
+        'playlist', 'pl',
+        'song', 'so'
+    )
+    arg_parser.add_argument(
         'action',
-        nargs='?',
         default=None,
-        choices=actions,
-        help='Choose one of: quick or q - download all songs from playlist defined under \'quickDownloadPlaylistLink\', playlist or pl - download all songs from provided URL, song or so - download a single song from provided URL.'
-    )  # todo maybe delete nargs. but hey it works
-    argParser.add_argument(
-        'url',
         nargs='?',
-        type=str,
-        help='Content URL.'
-    )  # todo maybe delete nargs. but hey it works
+        choices=actions,
+        help='''
+Choose one of:
+- quick or q - download all songs from playlist defined under \'quickDownloadPlaylistLink\',
+- playlist or pl - download all songs from provided URL,
+- song or so - download a single song from provided URL.
+ 
+'''
+    )
 
-    args = argParser.parse_args()
+    arg_parser.add_argument(
+        'url',
+        default=None,
+        nargs='?',
+        help='URL of the playlist or song to download.'
+    )
 
-    with open('config.json', 'r') as f:
-        config = json.load(f)
+    return arg_parser.parse_args()
+
+
+if __name__ == '__main__':
+    musicmanager_logger = setup_logger()
+    args = get_args()
+    config = get_config()
 
     # download by config file, default functionality
     if not args.action:
-        # main playlists
-        Playlist.savePathRoot = config['savePathRoot']
-
         for playlist in config['playlist']:
-            mainPlaylist = Playlist(playlist['link'], downloadWholePlaylist=True, name=playlist['name'])
-            mainPlaylist.download()
-            mainPlaylist.fixAlbumArt()
+            config_download = Playlist(
+                playlist['link'],
+                save_path_root=config['savePathRoot'],
+                download_whole_playlist=True, name=playlist['name']
+            )
+            config_download.download()
+            config_download.fix_album_art()
 
     # download quick download playlist
     elif args.action == 'quick' or args.action == 'q':
-        Playlist.savePathRoot = './'
-        quickDownload = Playlist(config['quickDownloadPlaylistLink'], downloadWholePlaylist=True)
-        quickDownload.download()
-        quickDownload.fixAlbumArt()
+        quick_playlist_download = Playlist(config['quickDownloadPlaylistLink'], download_whole_playlist=True)
+        quick_playlist_download.download()
+        quick_playlist_download.fix_album_art()
 
     elif args.url:
         # download custom playlist
         if args.action == 'playlist' or args.action == 'pl':
-            Playlist.savePathRoot = './'
-            quickDownload = Playlist(args.url, downloadWholePlaylist=True)
-            quickDownload.download()
-            quickDownload.fixAlbumArt()
+            custom_playlist_download = Playlist(args.url, download_whole_playlist=True)
+            custom_playlist_download.download()
+            custom_playlist_download.fix_album_art()
 
         # download custom song
         elif args.action == 'song' or args.action == 'so':
-            Playlist.savePathRoot = './'
-            quickDownload = Playlist(args.url, downloadWholePlaylist=False)
-            quickDownload.download()
-            quickDownload.fixAlbumArt()
+            custom_song_download = Playlist(args.url, download_whole_playlist=False)
+            custom_song_download.download()
+            custom_song_download.fix_album_art()
 
     elif not args.url:
-        raise Exception('URL not provided.')
-
+        musicmanager_logger.error('URL must be provided.')
     else:
-        raise Exception('Malformed command/args.')
+        musicmanager_logger.error('Malformed command/args.')
